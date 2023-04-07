ea07444d9fb887b753aecec9f5430fee8bff6d30
micoromano <marco.camelin@gmail.com>
2023-04-07 16:21:10 +0200

    upodate

--- scripts/usersLoad/UsersLoadData.py
+++ scripts/usersLoad/UsersLoadData.py
@@ -6,14 +6,14 @@ import boto3
 def load_users(users):
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table('Items')
-    for movie in movies:
-        id = int(movie['id'])
-        title = movie['info']
+    for user in users:
+        id = int(user['id'])
+        title = user['info']
         print("Adding user:", id, title)
-        table.put_item(Item=movie)
+        table.put_item(Item=user)
 
 
 if __name__ == '__main__':
     with open("userdata.json") as json_file:
-        movie_list = json.load(json_file, parse_float=Decimal)
-    load_users(movie_list)
+        user_list = json.load(json_file, parse_float=Decimal)
+    load_users(user_list)
